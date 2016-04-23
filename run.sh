d38453ad9b99be1f0eedb3f3f73d47ec86f85dae
Toma Luca <toma.luca95@gmail.com>
2016-04-23 23:12:19 +0000

    added group support

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [[ "$VNCPASS" == "" ]] ; then
+if [[ "$AKHETBASE_VNCPASS" == "" ]] ; then
+ echo "Missing AKHETBASE_VNCPASS"
  exit 1
 fi
 
@@ -8,21 +9,34 @@ fi
 python /websockify/websockify.py 6080 127.0.0.1:5900 -v --cert=/self.pem --key=/self.pem -D
 
 mkdir /root/.vnc/
-x11vnc -storepasswd $VNCPASS /root/.vnc/passwd
+x11vnc -storepasswd $AKHETBASE_VNCPASS /root/.vnc/passwd
 
 Xorg +extension GLX +extension RANDR +extension RENDER  -config /etc/X11/xorg.conf :0 &
 
-/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+if [[ "$AKHETBASE_USER_LABEL" != "" ]] ; then
+    usermod -c "$AKHETBASE_USER_LABEL" user
+fi
+
+for gid in $AKHETBASE_GIDs ; do
+    getent group $gid || groupadd -g $gid g$gid
+    adduser user $(getent group $gid | awk -F":" '{ print $1 }')
+done
+
+usermod -u $AKHETBASE_UID user
 
 chown user:user /home/user
 
-if [[ "$SHARED" == "1" ]] ; then
+usermod -l $AKHETBASE_USER user
+
+/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+
+if [[ "$AKHETBASE_SHARED" == "1" ]] ; then
  SHARED="-shared"
 else
-  SHARED="-nevershared -dontdisconnect"
+ SHARED="-nevershared -dontdisconnect"
 fi
 
-if [[ "$NOTIMEOUT" != "1" ]] ; then
+if [[ "$AKHETBASE_NOTIMEOUT" != "1" ]] ; then
  for i in $(seq 1 3) ; do
   x11vnc -rfbport 5900 -usepw -display :0 -shared -noxdamage -xrandr $SHARED -timeout 60 -ping 1 -repeat
  done
